77251519141a0d9ee2ba4ba5539e78dd1a04a691
onrush <moniruzzaman.eng1995@gmail.com>
2020-06-09 18:44:54 +0300

    Make a validation for the email

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ function showSuccess(input){
     formControl.className = "form-control success";
 }
 
+// Check email is valid
+function isValidEmail(email){
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 // Event Listener
 form.addEventListener("submit",function(e){
     e.preventDefault();
@@ -30,7 +36,10 @@ form.addEventListener("submit",function(e){
 
     if(email.value === ""){
         showError(email, "Email is required");
-    } else{
+    } else if(!isValidEmail(email.value)){
+        showError(email, "Email is not valid");
+    }
+    else{
         showSuccess(email);
     }
 
