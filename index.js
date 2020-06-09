3ca323d21eb948a6907157689fbef97cd214c941
onrush <moniruzzaman.eng1995@gmail.com>
2020-06-09 13:02:23 +0300

    Pass the error and success function through the form validation

--- index.js
+++ index.js
@@ -4,12 +4,29 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+// Show input error message
+function showError(input,message){
+    const formControl = input.parentElement;
+    formControl.className = "form-control error";
+    const smalll = formControl.querSelector("small");
+    smalll.innerText = message;
+}
+
+// Show success outline 
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+// Event Listener
 form.addEventListener("submit",function(e){
     e.preventDefault();
     console.log("submit");
 
     if(username.value === ""){
-        alert("username is required");
+        showError(username, "username is required");
+    } else{
+        showSuccess(username);
     }
 
     
